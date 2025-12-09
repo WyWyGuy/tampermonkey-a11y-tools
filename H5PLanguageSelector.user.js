816621f1034e4d425155b67d5c220f4b660e98b7
WyWyGuy <64562729+WyWyGuy@users.noreply.github.com>
2025-12-09 13:42:06 -0700

    Update H5PLanguageSelector.user.js

--- H5PLanguageSelector.user.js
+++ H5PLanguageSelector.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         H5P Language Selector
 // @namespace    http://tampermonkey.net/
-// @version      2025-12-04
-// @description  Scroll to a certain language in the H5P dropdown menu, or use ctrl + q to apply it automatically (robust version)
+// @version      2025-12-09
+// @description  Scroll to a certain language in the H5P dropdown menu, or use ctrl + q to apply it automatically
 // @author       Wyatt Nilsson
 // @match        https://byu.h5p.com/*
 // @icon         https://i0.wp.com/www.aufieroinformatica.com/wp-content/uploads/sites/7/2025/06/h5p_logo.png?fit=195%2C195&ssl=1
